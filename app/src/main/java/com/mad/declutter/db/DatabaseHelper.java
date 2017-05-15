e09989503855968e0338505f8a9eb85b3fbd2362
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-05-15 18:18:52 +1000

    Add new methods in DB helper to insert data, and fetch friends

--- app/src/main/java/com/mad/declutter/db/DatabaseHelper.java
+++ app/src/main/java/com/mad/declutter/db/DatabaseHelper.java
@@ -18,9 +18,13 @@ package com.mad.declutter.db;
 
 import android.content.Context;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.mad.declutter.model.Relationship;
+
+import twitter4j.Status;
 import twitter4j.User;
 
 /**
@@ -37,14 +41,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      * This method returns an instance of the database helper
      *
      * @param context Context
-     * @return sInstance TwitterDatabaseHelper
+     * @return sInstance DatabaseHelper
      */
-    public static synchronized TwitterDatabaseHelper getInstance(Context context) {
+    public static synchronized DatabaseHelper getInstance(Context context) {
         // Use the application context, which will ensure that you
         // don't accidentally leak an Activity's context.
         // See this article for more information: http://bit.ly/6LRzfx
         if (sInstance == null) {
-            sInstance = new TwitterDatabaseHelper(context.getApplicationContext());
+            sInstance = new DatabaseHelper(context.getApplicationContext());
         }
         return sInstance;
     }
@@ -53,7 +57,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      * Constructor should be private to prevent direct instantiation.
      * make call to static method "getInstance()" instead.
      */
-    private TwitterDatabaseHelper(Context context) {
+    private DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -97,12 +101,64 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onUpgrade(db, oldVersion, newVersion);
     }
 
-    public void insertTwitterUser(SQLiteDatabase db, User user) {
+    public void insertUser(SQLiteDatabase db, User user) {
         ContentValues values = new ContentValues();
-        values.put(UserSchema, arguments[0]);
-        values.put("Column2", arguments[1]);
-        values.put("Column3", arguments[2]);
+        values.put(UserSchema.COLUMN_ID, user.getId());
+        values.put(UserSchema.COLUMN_NAME, user.getName());
+        values.put(UserSchema.COLUMN_SCREEN_NAME, user.getScreenName());
+        values.put(UserSchema.COLUMN_DESCRIPTION, user.getDescription());
+        values.put(UserSchema.COLUMN_PROFILE_IMAGE, user.getProfileImageURLHttps());
+        values.put(UserSchema.COLUMN_FRIENDS_COUNT, user.getFriendsCount());
+        values.put(UserSchema.COLUMN_FOLLOWERS_COUNT, user.getFollowersCount());
+        values.put(UserSchema.COLUMN_STATUSES_COUNT, user.getStatusesCount());
+        values.put(UserSchema.COLUMN_CREATED_AT, user.getCreatedAt().toString());
 
-        db.insert(TABLE_NAME, null, values);
+        db.insertWithOnConflict(UserSchema.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+    }
+
+    public void insertRelationship(SQLiteDatabase db, Relationship relationship) {
+        ContentValues values = new ContentValues();
+        values.put(RelationshipSchema.COLUMN_USER_ID, relationship.getUserId());
+        values.put(RelationshipSchema.COLUMN_TARGET_USER_ID, relationship.getTargetUserId());
+        values.put(RelationshipSchema.COLUMN_FOLLOWS, relationship.isFollows());
+        values.put(RelationshipSchema.COLUMN_updated_at, relationship.getUpdatedAt());
+
+        db.insertWithOnConflict(RelationshipSchema.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+    }
+
+    public void insertStatus(SQLiteDatabase db, Status status) {
+        ContentValues values = new ContentValues();
+        values.put(StatusSchema.COLUMN_ID, status.getId());
+        values.put(StatusSchema.COLUMN_CREATED_AT, status.getCreatedAt().toString());
+        values.put(StatusSchema.COLUMN_LANG, status.getLang());
+        values.put(StatusSchema.COLUMN_FAVOURITE_COUNT, status.getFavoriteCount());
+        values.put(StatusSchema.COLUMN_RETWEET_COUNT, status.getRetweetCount());
+        values.put(StatusSchema.COLUMN_RETWEET, (status.getRetweetedStatus() != null));
+        values.put(StatusSchema.COLUMN_REPLY_SCREEN_NAME, status.getInReplyToScreenName());
+        values.put(StatusSchema.COLUMN_REPLY_USER_ID, status.getInReplyToUserId());
+        values.put(StatusSchema.COLUMN_REPLY_STATUS_ID, status.getInReplyToStatusId());
+        values.put(StatusSchema.COLUMN_TEXT, status.getText());
+        values.put(StatusSchema.COLUMN_SOURCE, status.getSource());
+        values.put(StatusSchema.COLUMN_USER_ID, status.getUser().getId());
+
+        db.insertWithOnConflict(StatusSchema.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+    }
+
+    public Cursor getFriends(SQLiteDatabase db, long currentUserId) {
+        String userTable = UserSchema.TABLE_NAME;
+        String relTable = RelationshipSchema.TABLE_NAME;
+
+
+        String userId = userTable + "." + UserSchema.COLUMN_ID;
+        String screenName = userTable + "." + UserSchema.COLUMN_SCREEN_NAME;
+        String relUserId = relTable + "." + RelationshipSchema.COLUMN_USER_ID;
+        String relTargetId = relTable + "." + RelationshipSchema.COLUMN_TARGET_USER_ID;
+
+        return db.rawQuery("SELECT " + userId + ", " + screenName +
+                " FROM " + userTable +
+                " JOIN " + relTable + "" +
+                " ON " + userId + " = " + relTargetId +
+                " WHERE " + relUserId + " = ?" +
+                " LIMIT 100", new String[] { String.valueOf(currentUserId) });
     }
 }
