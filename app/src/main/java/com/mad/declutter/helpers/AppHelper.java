bcc7e7bda14168565a419902fb341c4a95d6c546
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-05-15 18:20:45 +1000

    Put chunkArray in AppHelper (generic application helper)

--- app/src/main/java/com/mad/declutter/helpers/AppHelper.java
+++ app/src/main/java/com/mad/declutter/helpers/AppHelper.java
@@ -1,8 +1,22 @@
 package com.mad.declutter.helpers;
 
-/**
- * Created by Abdel on 15/5/17.
- */
-
 public class AppHelper {
+
+    private AppHelper() { }
+
+    public static long[][] chunkArray(long[] array, int chunkSize) {
+        int numOfChunks = (int)Math.ceil((double)array.length / chunkSize);
+        long[][] output = new long[numOfChunks][];
+
+        for(int i = 0; i < numOfChunks; ++i) {
+            int start = i * chunkSize;
+            int length = Math.min(array.length - start, chunkSize);
+
+            long[] temp = new long[length];
+            System.arraycopy(array, start, temp, 0, length);
+            output[i] = temp;
+        }
+
+        return output;
+    }
 }
