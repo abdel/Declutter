791609a221601088a135a12f0fec67a7709f1ceb
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-05-22 22:50:45 +1000

    Remove friendadapter from favourites activity

--- app/src/main/java/com/mad/declutter/activity/FavouritesActivity.java
+++ app/src/main/java/com/mad/declutter/activity/FavouritesActivity.java
@@ -3,17 +3,10 @@ package com.mad.declutter.activity;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.mad.declutter.R;
-import com.mad.declutter.adapter.FriendAdapter;
 import com.mad.declutter.db.DatabaseHelper;
-
-import java.util.ArrayList;
-
-import twitter4j.User;
 
 /**
  * Mobile Application Development - Exercise 6
@@ -24,7 +17,6 @@ public class FavouritesActivity extends AppCompatActivity {
     private SQLiteDatabase mDbRead;
     private DatabaseHelper mDbHelper;
     private RecyclerView mRecyclerView;
-    private FriendAdapter mFriendAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
